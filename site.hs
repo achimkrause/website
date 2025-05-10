71563bd8d4d2266cb1a50efa210455f0a21f05de
Achim Krause <achim.t.krause@gmail.com>
2025-05-10 21:12:32 +0200

    tikz integration working

--- site.hs
+++ site.hs
@@ -1,9 +1,25 @@
---------------------------------------------------------------------------------
+------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
 import Text.Pandoc.Options
+import Text.Pandoc.Definition
+import Text.Pandoc.Walk (walk, walkM)
 import Text.Pandoc.Highlighting (pygments)
+import Debug.Trace (traceShowId)
+
+import qualified Text.HTML.TagSoup as TS (parseTags, renderTags, Tag(TagOpen))
+
+import Control.Monad ((>=>))
+
+import Data.ByteString.Lazy.Char8 (pack, unpack, ByteString)
+import qualified Network.URI.Encode as URI (encode)
+import qualified Data.Text as Text
+
+import Data.String (fromString)
+
+import Data.Functor ((<&>))
+
 
 
 --------------------------------------------------------------------------------
@@ -18,6 +34,9 @@ writerOptions = defaultHakyllWriterOptions
   { writerHighlightStyle   = Just pygments
   , writerHTMLMathMethod = MathJax ""
   }
+
+myPandocCompiler =
+  pandocCompilerWithTransformM readerOptions writerOptions $ walkM tikzFilter
 
 main :: IO ()
 main = hakyllWith config $ do
@@ -39,13 +58,13 @@ main = hakyllWith config $ do
 
     match (fromList ["index.md", "about.md", "writing.md"]) $ do
         route   $ setExtension "html"
-        compile $ pandocCompilerWith readerOptions writerOptions
+        compile $ myPandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompilerWith readerOptions writerOptions
+        compile $ myPandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
@@ -80,10 +99,45 @@ main = hakyllWith config $ do
 
     match "templates/*" $ compile templateCompiler
 
-
---------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+tikzFilterWith :: FilePath -> SizeOption -> Block -> Compiler Block
+tikzFilterWith templatename size (CodeBlock (id, classes, namevals) contents) =
+ --(imageBlock . ("data:image/svg+xml;utf8," ++) . URI.encode . filter (/= '\n') . itemBody <$>) $
+  makeItem (Text.unpack contents)
+    >>= loadAndApplyTemplate (fromFilePath templatename) (bodyField "body")
+    <&> itemBody
+    <&> pack
+    >>= unixFilterLBS "rubber-pipe" ["--pdf"]
+    >>= unixFilterLBS "pdftocairo" ["-svg", "-", "-"]
+    <&> unpack
+    <&> filter (/= '\n')
+    <&> imageBlockFromSVG size id classes namevals
+tikzFilterWith _ _ x = return x
+
+tikzFilter :: Block -> Compiler Block
+tikzFilter cb@(CodeBlock (_, "tikzpicture":_, _) _) = tikzFilterWith "templates/tikzpicture.tex" FreeSize cb
+tikzFilter cb@(CodeBlock (_, "tikzcd":_, _) _) = tikzFilterWith "templates/tikzcd.tex" (FixedScale 0.2 "em") cb
+tikzFilter x = return x
+
+data SizeOption = FreeSize | FixedScale Float String
+
+imageBlockFromSVG :: SizeOption -> Text.Text -> [Text.Text] -> [(Text.Text,Text.Text)] -> String -> Block
+imageBlockFromSVG size id classes namevals code =
+        let dataurl = "data:image/svg+xml;utf8," ++ (URI.encode code)
+            tags = TS.parseTags code
+            findSvgTagAttrs [] = error "malformed SVG"
+            findSvgTagAttrs (TS.TagOpen "svg" attrs : _) = attrs
+            findSvgTagAttrs (_:rest) = findSvgTagAttrs rest
+            sizeAttrs = [(name,val) | (name,val) <- findSvgTagAttrs tags, name `elem` ["height", "width"]]
+            resize scale unit val = let (no,_):_ = reads val
+                                     in show (scale * no :: Float) ++ unit
+            extraAttrs = case size of
+                          FreeSize -> []
+                          FixedScale scale unit -> [(fromString name, fromString (resize scale unit val)) | (name,val) <- sizeAttrs]
+         in Para [Image (id, classes, extraAttrs ++ namevals) [] (fromString dataurl, "")]
+ 
 
